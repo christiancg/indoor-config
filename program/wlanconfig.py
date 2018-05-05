b2fa67ba31e9b83fed1428dc6d45dba805eee755
Christian Coello Grassi <christian.coello.g@gmail.com>
2018-05-05 13:21:27 -0300

    se arregla el endpoint para conectarse al wifi, pero hay que reiniciar del todo el raspberry

--- program/wlanconfig.py
+++ program/wlanconfig.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 from wifi import Cell, Scheme
 import json
@@ -9,6 +10,7 @@ class WlanConfig:
 	#~ wpasupplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
 	
 	OK = "ok"
+	HARD_RESET = "requires_hard_reset"
 	BAD_REQUEST = "bad_request"
 	ERROR = "error"
 	
@@ -43,6 +45,7 @@ class WlanConfig:
 					auxSecurity = item.encryption_type
 				auxResult = { "ssid": item.ssid, "security_type": auxSecurity }
 				result.append(auxResult)
+			print(json.dumps(result))
 			return json.dumps(result)
 		except Exception, ex:
 			import traceback
@@ -72,12 +75,12 @@ class WlanConfig:
 			if cell:
 				passkey = objReq['password']
 				self._delete('indoor')
-				scheme = Scheme.for_cell('wlan0', 'indoor', cell, passkey)
-				scheme.save()
+				#scheme = Scheme.for_cell('wlan0', 'indoor', cell, passkey)
+				#scheme.save()
 				#~ scheme.activate()
-				subprocess.call(["sudo", "ifdown", "wlan0"])
-				subprocess.call(["sudo", "ifup", "wlan0"])
-				return self.OK
+				scheme = SchemeWPA('wlan0',cell.ssid,{"ssid":cell.ssid,"psk":passkey})
+				scheme.save()
+				return self.HARD_RESET
 			else:
 				return self.BAD_REQUEST
 		except Exception, ex:
@@ -125,3 +128,143 @@ class WlanConfig:
 			cell.delete()
 			return True
 		return False
+
+
+
+import wifi.subprocess_compat as subprocessWPA
+from wifi.utils import ensure_file_exists
+
+class SchemeWPA(Scheme):
+
+    interfaces = "/etc/wpa_supplicant/wpa_supplicant.conf"
+
+    def __init__(self, interface, name, options=None):
+        self.interface = interface
+        self.name = name
+        self.options = options or {} 
+
+    def __str__(self):
+        """
+        Returns the representation of a scheme that you would need
+        in the /etc/wpa_supplicant/wpa_supplicant.conf file.
+        """
+
+        options = ''.join("\n    {k}=\"{v}\"".format(k=k, v=v) for k, v in self.options.items())
+        return "network={" + options + '\n}\n'
+
+    def __repr__(self):
+            return 'Scheme(interface={interface!r}, name={name!r}, options={options!r}'.format(**vars(self))
+    def save(self):
+        """
+        Writes the configuration to the :attr:`interfaces` file.
+        """
+        if not self.find(self.interface, self.name):
+            with open(self.interfaces, 'a') as f:
+                f.write('\n')
+                f.write(str(self))        
+
+    @classmethod
+    def all(cls):
+        """
+        Returns an generator of saved schemes.
+        """
+        ensure_file_exists(cls.interfaces)
+        with open(cls.interfaces, 'r') as f:
+            return extract_schemes(f.read(), scheme_class=cls) 
+    def activate(self):
+        """
+        Connects to the network as configured in this scheme.
+        """
+
+        subprocessWPA.check_output(['/sbin/ifdown', self.interface], stderr=subprocessWPA.STDOUT)
+        ifup_output = subprocessWPA.check_output(['/sbin/ifup', self.interface] , stderr=subprocessWPA.STDOUT)
+        ifup_output = ifup_output.decode('utf-8')
+
+        return self.parse_ifup_output(ifup_output)
+    def delete(self):
+        """
+        Deletes the configuration from the /etc/wpa_supplicant/wpa_supplicant.conf file.
+        """
+        content = ''
+        with open(self.interfaces, 'r') as f:
+            lines=f.read().splitlines()
+            while lines:
+                line=lines.pop(0)
+
+                if line.startswith('#') or not line:
+                    content+=line+"\n"
+                    continue
+
+                match = scheme_re.match(line)
+                if match:
+                    options = {}
+                    ssid=None
+                    content2=line+"\n"
+                    while lines and lines[0].startswith(' '):
+                        line=lines.pop(0)
+                        content2+=line+"\n"
+                        key, value = re.sub(r'\s{2,}', ' ', line.strip()).split('=', 1)
+                        #remove any surrounding quotes on value
+                        if value.startswith('"') and value.endswith('"'):
+                            value = value[1:-1]
+                        #store key, value
+                        options[key] = value
+                        #check for ssid (scheme name)
+                        if key=="ssid":
+                            ssid=value
+                    #get closing brace        
+                    line=lines.pop(0)
+                    content2+=line+"\n"
+
+                    #exit if the ssid was not found so just add to content
+                    if not ssid:
+                        content+=content2
+                        continue
+                    #if this isn't the ssid then just add to content
+                    if ssid!=self.name:
+                        content+=content2
+
+                else:
+                    #no match so add content
+                    content+=line+"\n"
+                    continue
+
+        #Write the new content
+        with open(self.interfaces, 'w') as f:
+            f.write(content)    
+
+scheme_re = re.compile(r'network={\s?')
+
+
+#override extract schemes
+def extract_schemes(interfaces, scheme_class=SchemeWPA):
+    lines = interfaces.splitlines()
+    while lines:
+        line = lines.pop(0)
+        if line.startswith('#') or not line:
+            continue
+
+        match = scheme_re.match(line)
+        if match:
+            options = {}
+            interface="wlan0"
+            ssid=None
+
+            while lines and lines[0].startswith(' '):
+                key, value = re.sub(r'\s{2,}', ' ', lines.pop(0).strip()).split('=', 1)
+                #remove any surrounding quotes on value
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1]
+                #store key, value
+                options[key] = value
+                #check for ssid (scheme name)
+                if key=="ssid":
+                    ssid=value
+
+            #exit if the ssid was not found
+            if ssid is None:
+                continue
+            #create a new class with this info
+            scheme = scheme_class(interface, ssid, options)
+
+            yield scheme
